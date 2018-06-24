1cb5f02617d7b5f23d046d44a65af77079c76fa9
rnorm <zhang.nl@outlook.com>
2018-06-23 23:19:58 -0400

    ah

--- src/fmt_pytorch.py
+++ src/fmt_pytorch.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+import torch.nn.functional as F
 from torchsummary import summary
 import numpy as np
 import datetime
@@ -129,34 +130,35 @@ class FMT():
                 x_sensitive_train_batch = x_sensitive_train[index * batch_size:(index + 1) * batch_size]
 
                 X_train_tensor = Variable(torch.FloatTensor(X_train_batch), requires_grad=False)
-                X_test_tensor = Variable(torch.FloatTensor(X_test), requires_grad=False)
 
                 y_train_tensor = Variable(torch.FloatTensor(y_train_batch).long(), requires_grad=False)
-                y_test_tensor = Variable(torch.FloatTensor(y_test).long(), requires_grad=False)
                 x_sensitive_train_tensor = Variable(torch.FloatTensor(x_sensitive_train_batch).long(),
                                                     requires_grad=False)
-                x_sensitive_test_tensor = Variable(torch.FloatTensor(x_sensitive_test).long(), requires_grad=False)
 
-                # train predictor
-                self.optimizerPredictor.zero_grad()
+                # pdb.set_trace()
                 filter_out = self.netFilter(X_train_tensor)
                 predictor_out = self.netPredictor(filter_out)
-                # pdb.set_trace()
                 predictor_loss = self.predictorCriterion(predictor_out, y_train_tensor)
                 epoch_predictor_loss.append(predictor_loss.item())
+                self.optimizerPredictor.zero_grad()
                 predictor_loss.backward()
                 self.optimizerPredictor.step()
 
                 # train corrector
-                self.optimizerCorrector.zero_grad()
+                filter_out = self.netFilter(X_train_tensor)
                 corrector_out = self.netCorrector(filter_out)
                 corrector_loss = self.correctorCriterion(corrector_out, x_sensitive_train_tensor)
                 epoch_corrector_loss.append(corrector_loss.item())
+                self.optimizerCorrector.zero_grad()
                 corrector_loss.backward()
                 self.optimizerCorrector.step()
 
-                # train filter
+                # pdb.set_trace()
+
                 self.optimizerFilter.zero_grad()
+                filter_out = self.netFilter(X_train_tensor)
+                predictor_out = self.netPredictor(filter_out)
+                corrector_out = self.netCorrector(filter_out)
                 filter_loss = self.predictorCriterion(predictor_out, y_train_tensor) * self.weight[0] + \
                               self.correctorCriterion(corrector_out, x_sensitive_train_tensor) * self.weight[1]
                 epoch_filter_loss.append(filter_loss.item())
@@ -164,48 +166,63 @@ class FMT():
                 self.optimizerFilter.step()
                 # pdb.set_trace()
 
-            # print('Testing for epoch {}:'.format(epoch))
-            # logger.info('Testing for epoch {}:'.format(epoch))
-            #
-            # train_predictor_loss = self.predictor.evaluate(X_train, y_train,
-            #                                                verbose=False)  # np.mean(np.array(epoch_predictor_loss), axis=0)
-            # train_corrector_loss = self.corrector.evaluate(X_train, x_sensitive_train,
-            #                                                verbose=False)  # np.mean(np.array(epoch_predictor_loss), axis=0)
-            #
-            # test_predictor_loss = self.predictor.evaluate(X_test, y_test, verbose=False)
-            # test_corrector_loss = self.corrector.evaluate(X_test, x_sensitive_test, verbose=False)
-            #
-            # train_history['predictor'].append(train_predictor_loss)
-            # train_history['corrector'].append(train_corrector_loss)
-            #
-            # test_history['predictor'].append(test_predictor_loss)
-            # test_history['corrector'].append(test_corrector_loss)
+            print('Testing for epoch {}:'.format(epoch))
+            logger.info('Testing for epoch {}:'.format(epoch))
+            train_filter_out = self.netFilter(Variable(torch.FloatTensor(X_train), requires_grad=False))
+            test_filter_out = self.netFilter(Variable(torch.FloatTensor(X_test), requires_grad=False))
+            train_predictor_out = self.netPredictor(train_filter_out)
+            train_corrector_out = self.netCorrector(train_filter_out)
+            test_predictor_out = self.netPredictor(test_filter_out)
+            test_corrector_out = self.netCorrector(test_filter_out)
+
+            train_real_y = Variable(torch.FloatTensor(y_train).long(), requires_grad=False)
+            test_real_y = Variable(torch.FloatTensor(y_test).long(), requires_grad=False)
+
+            train_real_x_sensitive = Variable(torch.FloatTensor(x_sensitive_train).long(),
+                                              requires_grad=False)
+            test_real_x_sensitive = Variable(torch.FloatTensor(x_sensitive_test).long(),
+                                             requires_grad=False)
+
+            train_predictor_loss = train_predictor_out.max(1)[1]
+            # pdb.set_trace()
+            train_y_accuracy = train_predictor_loss.eq(train_real_y.view_as(train_real_y)).sum().item() / \
+                               train_real_y.shape[0]
+            train_y_loss = F.cross_entropy(train_predictor_out, train_real_y).item()
+
+            test_predictor_loss = test_predictor_out.max(1)[1]
+            test_y_accuracy = test_predictor_loss.eq(test_real_y.view_as(test_real_y)).sum().item() / test_real_y.shape[
+                0]
+            test_y_loss = F.cross_entropy(test_predictor_out, test_real_y).item()
+
+            train_corrector_loss = train_corrector_out.max(1)[1]
+            train_x_sensitive_accuracy = train_corrector_loss.eq(
+                train_real_x_sensitive.view_as(train_real_x_sensitive)).sum().item() / train_real_x_sensitive.shape[0]
+            train_x_sensitive_loss = F.cross_entropy(train_corrector_out, train_real_x_sensitive).item()
+
+            test_corrector_loss = test_corrector_out.max(1)[1]
+            test_x_sensitive_accuracy = test_corrector_loss.eq(
+                test_real_x_sensitive.view_as(test_real_x_sensitive)).sum().item() / train_real_x_sensitive.shape[0]
+            test_x_sensitive_loss = F.cross_entropy(test_corrector_out, test_real_x_sensitive).item()
+
+            train_history['predictor'].append(train_y_loss)
+            train_history['corrector'].append(train_x_sensitive_loss)
+
+            test_history['predictor'].append(train_y_loss)
+            test_history['corrector'].append(test_x_sensitive_loss)
             # # pdb.set_trace()
-            # print('{0:<22s} | {1:4s} | {2:15s} '.format(
-            #     'component', *self.predictor.metrics_names))
-            # print('-' * 60)
-            # logger.info('{0:<22s} | {1:4s} | {2:15s} '.format(
-            #     'component', *self.predictor.metrics_names))
-            # logger.info('-' * 60)
-            #
-            # ROW_FMT = '{0:<22s} | {1:<4.2f} | {2:<15.4f}'
-            # print(ROW_FMT.format('predictor (train)',
-            #                      *train_history['predictor'][-1]))
-            # print(ROW_FMT.format('predictor (test)',
-            #                      *test_history['predictor'][-1]))
-            # print(ROW_FMT.format('corrector (train)',
-            #                      *train_history['corrector'][-1]))
-            # print(ROW_FMT.format('corrector (test)',
-            #                      *test_history['corrector'][-1]))
-            #
-            # logger.info(ROW_FMT.format('predictor (train)',
-            #                            *train_history['predictor'][-1]))
-            # logger.info(ROW_FMT.format('predictor (test)',
-            #                            *test_history['predictor'][-1]))
-            # logger.info(ROW_FMT.format('corrector (train)',
-            #                            *train_history['corrector'][-1]))
-            # logger.info(ROW_FMT.format('corrector (test)',
-            #                            *test_history['corrector'][-1]))
+            print('{0:<22s} | {1:6s} | {2:15s} '.format(
+                'component', 'acc', 'cross entropy'))
+            print('-' * 60)
+
+            ROW_FMT = '{0:<22s} | {1:<4.4f} | {2:<15.4f}'
+            print(ROW_FMT.format('predictor (train)',
+                                 train_y_accuracy, train_y_loss))
+            print(ROW_FMT.format('predictor (test)',
+                                 test_y_accuracy, test_y_loss))
+            print(ROW_FMT.format('corrector (train)',
+                                 train_x_sensitive_accuracy, train_x_sensitive_loss))
+            print(ROW_FMT.format('corrector (test)',
+                                 test_x_sensitive_accuracy, test_x_sensitive_loss))
 
 
 def print_network(net):
@@ -233,7 +250,7 @@ if __name__ == '__main__':
 
     config = load_config()
     df = read_data(config, 'adult')
-    logger.info("{0}: shape of data read - {1}".format(datetime.datetime.now(), df.shape))
+    logger.info("{0}: shape of data read: {1}".format(datetime.datetime.now(), df.shape))
 
     sensitive_features = ['race', 'sex', 'race-sex']
     response = ['income-per-year']
